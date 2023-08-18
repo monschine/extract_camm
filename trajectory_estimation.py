ac0dd1189fbf8abb8cb7f60b6638b90e287178e4
Jordi Vallverdu <jordi.vallverdu.alcover@gmail.com>
2023-08-18 15:46:22 +0200

    small update on the non working trajectory estimation using kalman filters

--- trajectory_estimation.py
+++ trajectory_estimation.py
@@ -3,6 +3,10 @@ import numpy as np
 import json
 from scipy.integrate import cumtrapz
 import matplotlib.pyplot as plt
+import os
+
+import argparse
+
 
 class KalmanFilter:
     def __init__(self, process_variance, measurement_variance, estimate):
@@ -42,27 +46,51 @@ def integrate_trajectory_dx(angular_velocity, acceleration, dx):
     position = cumtrapz(velocity, dx=dx, initial=0)
     return position
 
-# Load and process data
-with open("/mnt/data/R0013304_0.json", 'r') as f:
-    data = json.load(f)
 
-angular_velocity_data = np.array([sample['angularvelocity'] for sample in data])
-acceleration_data = np.array([sample['acceleration'] for sample in data])
-time_data = np.array([sample['sampletime'] for sample in data])
+def main(inputPath, save):
 
-dx = data[0]['sampleduration']
-fused_angular_velocity, fused_acceleration = sensor_fusion(angular_velocity_data, acceleration_data, time_data)
-trajectory_scaled = integrate_trajectory_dx(fused_angular_velocity, fused_acceleration, dx)
+    # Load and process data
+    with open(inputPath, 'r') as f:
+        data = json.load(f)
 
-# Visualization
-fig_scaled = plt.figure(figsize=(10, 8))
-ax_scaled = fig_scaled.add_subplot(111, projection='3d')
-ax_scaled.plot(trajectory_scaled[:, 0], trajectory_scaled[:, 1], trajectory_scaled[:, 2], 
-               label='Scaled Estimated Trajectory', color='blue')
-ax_scaled.set_xlabel('X (meters)')
-ax_scaled.set_ylabel('Y (meters)')
-ax_scaled.set_zlabel('Z (meters)')
-ax_scaled.set_title('3D Scaled Trajectory Estimation')
-ax_scaled.legend()
-plt.show()
+    angular_velocity_data = np.array([sample['angularvelocity'] for sample in data])
+    acceleration_data = np.array([sample['acceleration'] for sample in data])
+    time_data = np.array([sample['sampletime'] for sample in data])
 
+    dx = data[0]['sampleduration']
+    fused_angular_velocity, fused_acceleration = sensor_fusion(angular_velocity_data, acceleration_data, time_data)
+    trajectory_scaled = integrate_trajectory_dx(fused_angular_velocity, fused_acceleration, dx)
+
+    # Visualization
+    fig_scaled = plt.figure(figsize=(10, 8))
+    ax_scaled = fig_scaled.add_subplot(111, projection='3d')
+    ax_scaled.plot(trajectory_scaled[:, 0], trajectory_scaled[:, 1], trajectory_scaled[:, 2], 
+                label='Scaled Estimated Trajectory', color='blue')
+    ax_scaled.set_xlabel('X (meters)')
+    ax_scaled.set_ylabel('Y (meters)')
+    ax_scaled.set_zlabel('Z (meters)')
+    ax_scaled.set_title('3D Scaled Trajectory Estimation')
+    ax_scaled.legend()
+
+    if save :
+        # Get the file name without extension
+        file_name_without_extension = os.path.splitext(os.path.basename(inputPath))[0]
+
+        # Get the directory of the video file and create the .txt file path
+        directory = os.path.dirname(inputPath)
+
+        plt.savefig(os.path.join(directory, f"{file_name_without_extension}_plot.png"))
+    
+    else :
+        plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='perform sensor fusion on ricoh theta IMU data')
+
+    parser.add_argument('-i' ,'--inputPath',    default='./video_samples/R0013405_1.json',  type=str, help='Path to the IMU data in json format')
+    parser.add_argument('-s', '--save',         action='store_true', help='save the plot')
+
+    args = parser.parse_args()
+
+    main(args.inputPath, args.save)
